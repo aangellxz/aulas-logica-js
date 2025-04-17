125b4b060c59d73efc808a105bb81ea6284e8efe
ÂNGELA DOS SANTOS AGUIAR <angela_s_aguiar@estudante.sesisenai.org.br>
2025-04-16 22:17:48 -0300

    Inico da aula sobre JS

---
+++ variaveis.js
@@ -0,0 +1,17 @@
+// declaração de variavel com var
+
+/*var nome ="Ângela"
+
+var nome = "João"*/
+
+// declaração de variavel com let
+
+// let nome = "Ângela"
+
+// nome = "João"
+
+// const nome = "Ângela"
+
+// nome = "João"
+
+console.log(nome)
