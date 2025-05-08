f86ba69eb4050393d392c83665caa3ea3ceb5417
ÂNGELA DOS SANTOS AGUIAR <angela_s_aguiar@estudante.sesisenai.org.br>
2025-05-07 22:18:08 -0300

    feat: Exercícos de JS

--- DOM/eventos-mouse/contador/scripts.js
+++ DOM/eventos-mouse/contador/scripts.js
@@ -3,8 +3,19 @@ let contador = 0
 const resultado = document.querySelector("#resultado")
 
 const btnIncrementaValor = document.querySelector("#btnIncrementar")
+const btnDiminuiValor = document.querySelector("#btnDiminuiValor")
 
 btnIncrementaValor.addEventListener("click", ()=>{
     contador++
     resultado.textContent = `Contador: ${contador}`
+})
+
+
+
+btnDiminuiValor.addEventListener("click",()=>{
+    if(contador > 0){
+            contador--
+    resultado.textContent = `Contador: ${contador}`
+    }
+
 })
